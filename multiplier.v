5d8519048d2afd6b97fd54821a84c0b87eb82f54
Bhoomika Kongi <102047163+Bhoomikakongi@users.noreply.github.com>
2022-03-21 23:48:25 +0530

    TESTBENCH

--- multiplier.v
+++ multiplier.v
@@ -79,3 +79,35 @@ endcase
 end 
 endmodule
 
+
+ 
+ module multb;
+reg [15:0]data_in;
+reg clk,start;
+wire done;
+wire [15:0] x,y,z,bout,bus;
+MUL_datapath DP(eqz,lda,ldb,ldp,clr,decb,data_in,clk);
+controller con(lda,ldb,ldp,clrp,decb,clk,start);
+initial
+begin
+clk=1'b0;
+#3 start=1'b1;
+#500 $finish;
+end
+
+always #5 clk=~clk;
+
+initial 
+begin
+#17 data_in=17;
+#10 data_in=5;
+end
+
+initial 
+begin
+$monitor ($time,"%d %b",dp.y,done);
+$dumpfile("repmul.vcd");
+$dumpvars(0,multb);
+
+end
+endmodule
